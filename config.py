bee9e2a77fc7bcef112d2c0473107239eafafcd6
Bruno Miranda <bruno.silva@catho.com>
2020-02-05 01:38:35 -0300

    feat(HT):[BR] configure db sqlite - ORM

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os.path
+
+
 DEBUG = True
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'storage.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
